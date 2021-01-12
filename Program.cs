ec97667aee1218819b3f9b27f6a09ed00bbac4c0
unknown <pedrofelipbarros@gmail.com>
2021-01-12 13:19:22 -0300

    exrcicio6 23.11 finalizado

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace exercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string [5];
+            string busca;
+
+            Console.WriteLine("Escreva os nomes a baixo:");
+            for (var i = 0; i <5; i++)
+            {
+                Console.WriteLine($"{i+1}° nome:");
+                nomes[i] = Console.ReadLine();
+            }
+            
+            Console.WriteLine("Digite o nome para busca no sistema:");
+            busca = Console.ReadLine();
+            for (var i = 0; i <1; i++)
+            {
+            if (busca == nomes[i])
+            {
+                Console.WriteLine("ACHEI!!!");
+            }
+            else
+            {
+                Console.WriteLine("Não achei :(");
+            }
+            }
+
+        }
+    }
+}
